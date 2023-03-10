232279381ba42a752be41a6a037e11873deff2bc
Shai Rubin <shai@ownbackup.com>
2023-03-10 12:20:13 +0200

    source match test passes 135 136 137 141 tests passed 144 tests pass 146 tests pass 148 tests passed
    added withitem matcher
    two additional test for ast_with are passing
    155 tests passing

--- py-ast-utils/source_match.py
+++ py-ast-utils/source_match.py
@@ -1750,7 +1750,7 @@ class WithItemSourceMatcher(SourceMatcher):
             'optional_vars',
             before_placeholder=TextPlaceholder(r' *as *', ' as '))
 
-    #    self.compound_separator = TextPlaceholder(r'\s*,\s*', ', ')
+        #self.compound_separator = TextPlaceholder(r'\s*,\s*', ', ')
 
     def Match(self, string):
         #    if 'as' not in string:
@@ -1787,12 +1787,12 @@ class WithSourceMatcher(SourceMatcher):
     def __init__(self, node, starting_parens=None):
         super(WithSourceMatcher, self).__init__(node, starting_parens)
         self.with_placeholder = TextPlaceholder(r' *(with)? *', 'with ')
-        self.withitems_placeholder = ListFieldPlaceholder('items')
+        self.withitems_placeholder = SeparatedListFieldPlaceholder('items',separator_placeholder=TextPlaceholder(r', *', ', '))
         #    self.context_expr = FieldPlaceholder('context_expr')
         #    self.optional_vars = FieldPlaceholder(
         #        'optional_vars',
         #        before_placeholder=TextPlaceholder(r' *as *', ' as '))
-        self.compound_separator = TextPlaceholder(r'\s*,\s*', ', ')
+#        self.compound_separator = TextPlaceholder(r'\s*,\s*', ', ')
         self.colon_placeholder = TextPlaceholder(r':\n?', ':\n')
         self.body_placeholder = BodyPlaceholder('body')
         self.is_compound_with = False
