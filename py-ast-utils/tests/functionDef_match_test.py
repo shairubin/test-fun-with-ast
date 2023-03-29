b33b94da1224fd236c798bdd578f6ad04102cc4e
Shai Rubin <shai@ownbackup.com>
2023-03-29 05:56:54 +0300

    99 tests passed 1 ignored functionDef to a seperate file

---
+++ py-ast-utils/tests/functionDef_match_test.py
@@ -0,0 +1,118 @@
+import unittest
+
+import create_node
+import source_match
+
+
+class FunctionDefMatcherTest(unittest.TestCase):
+
+    def testEmpty(self):
+        node = create_node.FunctionDef('test_fun', body=[create_node.Pass()])
+        string = 'def test_fun():\n\t\t\t\tpass\n'
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        self.assertEqual(string, matcher.GetSource())
+
+    def testSingleArg(self):
+        node = create_node.FunctionDef('test_fun', create_node.arguments(args=['a']), body=[create_node.Pass()])
+        string = 'def test_fun(a):\n  pass\n'
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        self.assertEqual(string, matcher.GetSource())
+
+    def testMultipleArgs(self):
+        node = create_node.FunctionDef('test_fun', create_node.arguments(args=['a', 'b']), body=[create_node.Pass()])
+        string = 'def test_fun(a, b):\n  pass\n'
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        self.assertEqual(string, matcher.GetSource())
+
+    def testDefaultConstant(self):
+        #        node = create_node.FunctionDef('test_fun', keys=('a'), values=('b'))
+        node = create_node.FunctionDef(
+            'test_fun', create_node.arguments(args=['a'], defaults=[3]),
+            body=[create_node.Pass()])
+
+        string = "def test_fun(a=3):\npass\n"
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        matched_source = matcher.GetSource()
+        self.assertEqual(string, matched_source)
+
+    def testDefaults(self):
+        node = create_node.FunctionDef(
+            'test_fun', create_node.arguments(args=['e', 'f', 'a', 'c'], defaults=['b', 'd']),
+            body=[create_node.Pass()])
+
+        string = 'def test_fun(e, f, a =b, c= d):\n  pass\n'
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        self.assertEqual(string, matcher.GetSource())
+
+    def testArgsDefaultsVarargs(self):
+        # node = create_node.FunctionDef(
+        #     'test_fun', arcg=('e', 'f'), keys=('a', 'c'), values=('b', 'd'),
+        #     vararg_name='args')
+        node = create_node.FunctionDef(
+            'test_fun', create_node.arguments(args=['e', 'f', 'a', 'c'], defaults=['b', 'd'], vararg='d'),
+            body=[create_node.Pass()])
+
+        string = 'def test_fun(e, f, a=b, c=d, *d):\n  pass\n'
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        self.assertEqual(string, matcher.GetSource())
+
+    def testArgsDefaultsVarargsKwargs(self):
+        # node = create_node.FunctionDef(
+        #     'test_fun', args=('e', 'f'), keys=('a', 'c'), values=('b', 'd'),
+        #     vararg_name='args', kwarg_name='kwargs')
+        node = create_node.FunctionDef(
+            'test_fun', create_node.arguments(args=['e', 'f', 'a', 'c'], defaults=['b', 'd'], vararg='d', kwarg='a'),
+            body=[create_node.Pass()])
+
+        string = 'def test_fun(e, f, a=b, c=d, *d, **a):\n  pass\n'
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        self.assertEqual(string, matcher.GetSource())
+
+    def testDecoratorList(self):
+        node = create_node.FunctionDef(
+            'test_fun',
+            decorator_list=[create_node.Name('dec'),
+                            create_node.Call('call_dec')],
+            body=[create_node.Pass()])
+        string = '@dec\n@call_dec()\ndef test_fun():\n  pass\n'
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        self.assertEqual(string, matcher.GetSource())
+
+    def testCommentInDecoratorList(self):
+        node = create_node.FunctionDef(
+            'test_fun',
+            decorator_list=[create_node.Name('dec'),
+                            create_node.Call('call_dec')],
+            body=[create_node.Pass()])
+        string = '@dec\n#hello world\n@call_dec()\ndef test_fun():\n  pass\n'
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        self.assertEqual(string, matcher.GetSource())
+
+    def testCommentAfterDecorator(self):
+        node = create_node.FunctionDef(
+            'test_fun',
+            decorator_list=[create_node.Name('dec')],
+            body=[create_node.Pass()])
+        string = '@dec\n #comment\ndef test_fun():\n  pass\n'
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        matcher_result = matcher.GetSource()
+        self.assertEqual(string, matcher_result)
+
+    def testBody(self):
+        node = create_node.FunctionDef(
+            'test_fun',
+            body=(create_node.Expr(create_node.Name('a')),))
+        string = 'def test_fun():\n  a\n'
+        matcher = source_match.GetMatcher(node)
+        matcher.Match(string)
+        self.assertEqual(string, matcher.GetSource())
