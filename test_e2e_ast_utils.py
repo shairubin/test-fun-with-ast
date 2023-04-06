1c8770e30bb3656e6bb5ef61e444dc29b1e04440
Shai Rubin <shai@ownbackup.com>
2023-04-06 18:28:56 +0200

    first attempt to use the library

--- test_e2e_ast_utils.py
+++ test_e2e_ast_utils.py
@@ -6,11 +6,14 @@ from test_utils import TestUtils
 
 @pytest.fixture(scope="module",
 #                params=['./../test_programs/find_largest_number.py', './../test_programs/simple_print.py'])
-                params=['./../test_programs/simple_module.py',
-                        './../test_programs/simple_module2.py', \
-                        './../test_programs/simple_module3.py', \
-                        './../test_programs/simple_module_assignment.py', \
-                        './../test_programs/match_comment_eol.py'])
+                params=[
+                        #'./../test_programs/simple_module.py',
+                        #'./../test_programs/simple_module2.py', \
+                        #'./../test_programs/simple_module3.py', \
+                        #'./../test_programs/simple_module_assignment.py', \
+                        #'./../test_programs/match_comment_eol.py'
+                        './test_programs/simple_module.py'
+                        ])
 def test_program(request):
     yield request.param
 
@@ -22,7 +25,7 @@ class TestE2EASTUtils:
         python_code = test_utils.read_file_as_string(test_program)
         import ast
         module_node = ast.parse(python_code)
-        import source_match
+        import fun_with_ast.source_match
         source_match.GetSource(module_node, python_code)
         FunWithAst_code = module_node.matcher.GetSource()
         assert python_code == FunWithAst_code
