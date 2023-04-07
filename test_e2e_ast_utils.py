0b5b48e75ba8e22e6a0940d953faebb524e51f48
Shai Rubin <shai@ownbackup.com>
2023-04-07 10:44:37 +0200

    first version of autologging supports fun-with-ast 0.1.12

--- test_e2e_ast_utils.py
+++ test_e2e_ast_utils.py
@@ -1,6 +1,6 @@
 
 import pytest
-
+from fun_with_ast import source_match
 from test_utils import TestUtils
 
 
@@ -25,7 +25,6 @@ class TestE2EASTUtils:
         python_code = test_utils.read_file_as_string(test_program)
         import ast
         module_node = ast.parse(python_code)
-        import fun_with_ast.source_match
         source_match.GetSource(module_node, python_code)
         FunWithAst_code = module_node.matcher.GetSource()
         assert python_code == FunWithAst_code
