0315d0d3257bef5962949ce95f3109c8acff0269
Shai Rubin <shai@ownbackup.com>
2023-04-22 18:44:21 +0300

    first rewite test passing

--- rewrite/RewriteIf.py
+++ rewrite/RewriteIf.py
@@ -1,30 +1,34 @@
 import ast
 from ast import NodeTransformer
 import logging
+from dataclasses import dataclass
 
 from fun_with_ast import source_match
 from fun_with_ast.create_node import GetNodeFromInput
-from fun_with_ast.dynamic_matcher import GetDynamicMatcher
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
-from enum import auto, Flag
 
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=False)
+class IfRewrtiteConfig():
+    _body_log: str = None
 
-class IfRewrtiteAction(Flag):
-    LOG_IF_BODY = auto()
+    @property
+    def body_log(self):
+        return self._body_log
 
 class RewriteIf(NodeTransformer):
-    def __init__(self, if_action):
+    def __init__(self, if_config):
         super(NodeTransformer, self).__init__()
-        self._if_action = if_action
+        self._if_config = if_config
+
+        if not self._if_config.body_log:
+            raise NotImplementedError('empty body_log is not implemented')
+        self.body_log = self._if_config.body_log
 
     def visit_If(self, node):
-#        unparsted_if_node = ast.unparse(node.test)
-#        logging.info("visited if node. Test is: "+ unparsted_if_node)
         self._add_logs_to_if_node(node)
         result = self.generic_visit(node)
-#        logging.info("finish if node. Test is: "+ unparsted_if_node)
         return result
 
     def _add_logs_to_if_node(self, if_node):
@@ -41,76 +45,80 @@ class RewriteIf(NodeTransformer):
 
 
     def __generate_log_node(self, if_node):
-        formated_string  = self.__get_log_formated_string(if_node.test)
-        join_Str_string = 'f\" ' + formated_string + ' \"'
-        log_node = GetNodeFromInput('logger.info(\'test string\')\n')
-        #matcher = GetDynamicMatcher(log_node)
-        #matcher.Match('logger.info(\'test string\')')
-        source = source_match.GetSource(log_node, 'logger.info(\'test string\')\n')
-
-#        log_func = ast.Attribute(value=ast.Name(id='logging', ctx=ast.Load()),
-#                                 attr='info',
-#                                 ctx=ast.Load())
-        #JoinedStr_node = self.__get_JoinedStr_node(formatted_string)
-#        args = JoinedStr_node
-#        call = ast.Call(func=log_func, args=args, keywords=[])
-#        result = ast.Expr(value=call)
+        ident = ' '*self.__get_ident(if_node)
+        log_line = f'logger.info(\'{self.body_log}\')\n'
+        log_node = GetNodeFromInput(log_line)
+        source = source_match.GetSource(log_node, ident+log_line)
         return log_node
 
-    def __get_log_formated_string(self, if_condition):
-        if isinstance(if_condition, ast.Compare):
-            return self.__handle_compare(if_condition)
-        elif isinstance(if_condition, ast.BoolOp):
-            return self.__handle_boolop(if_condition)
-        elif isinstance(if_condition, ast.Call):
-            return self.__handle_call(if_condition)
-        elif isinstance(if_condition, ast.Name):
-            return ''+ f'{if_condition.id}' + ''
-        elif isinstance(if_condition, ast.Constant):
-            return str(if_condition.value)
-        else:
+    # def __get_log_formated_string(self, if_condition):
+    #     if isinstance(if_condition, ast.Compare):
+    #         return self.__handle_compare(if_condition)
+    #     elif isinstance(if_condition, ast.BoolOp):
+    #         return self.__handle_boolop(if_condition)
+    #     elif isinstance(if_condition, ast.Call):
+    #         return self.__handle_call(if_condition)
+    #     elif isinstance(if_condition, ast.Name):
+    #         return ''+ f'{if_condition.id}' + ''
+    #     elif isinstance(if_condition, ast.Constant):
+    #         return str(if_condition.value)
+    #     else:
+    #         raise NotImplementedError
+    #
+    # def __get_JoinedStr_node(self, formated_string):
+    #     result_string = 'f\" ' + formated_string + ' \"'
+    #     result = ast.parse(result_string)
+    #     logger.info(ast.unparse(result))
+    #     return [result.body[0].value]
+    #
+    # def __handle_compare(self, compare_node):
+    #     if len(compare_node.ops) == 1:
+    #         left =  self.__get_log_formated_string(compare_node.left)
+    #     else:
+    #         raise NotImplementedError
+    #     if len(compare_node.comparators) == 1:
+    #         comperators =  self.__get_log_formated_string(compare_node.comparators[0])
+    #     else:
+    #         raise NotImplementedError
+    #     op_str = self.__ops_to_string(compare_node.ops[0])
+    #     result =  left + op_str + comperators
+    #     return result
+    # def __handle_call(self, call_node):
+    #     args = ''
+    #     for arg in call_node.args:
+    #         args+= " "+self.__get_log_formated_string(arg)
+    #     result =  "called " + call_node.func.id + " with args: "+args
+    #     return result
+    #
+    # def __handle_boolop(self, boolop_node):
+    #     if len(boolop_node.values) == 2:
+    #         left =  self.__get_log_formated_string(boolop_node.values[0])
+    #         right = self.__get_log_formated_string(boolop_node.values[1])
+    #     else:
+    #         raise NotImplementedError
+    #     result =  left + " BoolOp " + right
+    #     return result
+    #
+    # def __ops_to_string(self, ast_op):
+    #     if isinstance(ast_op, ast.Eq):
+    #         return '='
+    #     elif isinstance(ast_op, ast.NotEq):
+    #         return '!='
+    #     elif isinstance(ast_op, ast.GtE):
+    #         return '>='
+    #     else:
+    #         raise NotImplementedError
+    def __get_ident(self, if_node):
+        if if_node.__getattribute__('orelse'):
             raise NotImplementedError
+        body = if_node.body
+        ident = 0
+        for stmt in body:
+            stmt_ident = stmt.col_offset
+            if stmt_ident > ident and ident==0:
+                ident = stmt_ident
+            elif stmt_ident > ident and ident!=0:
+                raise NotImplementedError
 
-    def __get_JoinedStr_node(self, formated_string):
-        result_string = 'f\" ' + formated_string + ' \"'
-        result = ast.parse(result_string)
-        logger.info(ast.unparse(result))
-        return [result.body[0].value]
+        return ident
 
-    def __handle_compare(self, compare_node):
-        if len(compare_node.ops) == 1:
-            left =  self.__get_log_formated_string(compare_node.left)
-        else:
-            raise NotImplementedError
-        if len(compare_node.comparators) == 1:
-            comperators =  self.__get_log_formated_string(compare_node.comparators[0])
-        else:
-            raise NotImplementedError
-        op_str = self.__ops_to_string(compare_node.ops[0])
-        result =  left + op_str + comperators
-        return result
-    def __handle_call(self, call_node):
-        args = ''
-        for arg in call_node.args:
-            args+= " "+self.__get_log_formated_string(arg)
-        result =  "called " + call_node.func.id + " with args: "+args
-        return result
-
-    def __handle_boolop(self, boolop_node):
-        if len(boolop_node.values) == 2:
-            left =  self.__get_log_formated_string(boolop_node.values[0])
-            right = self.__get_log_formated_string(boolop_node.values[1])
-        else:
-            raise NotImplementedError
-        result =  left + " BoolOp " + right
-        return result
-
-    def __ops_to_string(self, ast_op):
-        if isinstance(ast_op, ast.Eq):
-            return '='
-        elif isinstance(ast_op, ast.NotEq):
-            return '!='
-        elif isinstance(ast_op, ast.GtE):
-            return '>='
-        else:
-            raise NotImplementedError
--- rewrite/tests/rewrite_if_test.py
+++ rewrite/tests/rewrite_if_test.py
@@ -1,4 +1,7 @@
-from rewrite.RewriteIf import RewriteIf, IfRewrtiteAction
+import difflib
+from pprint import pprint
+
+from rewrite.RewriteIf import RewriteIf, IfRewrtiteConfig
 from rewrite.RewriteImports import RewriteImports
 import ast
 from fun_with_ast import source_match
@@ -8,20 +11,17 @@ class TestImportRewrite:
 
     def test_simple_if_rewrite(self):
         python_code = "if True:\n    a = 1"
-        module_node = self.__rewrite_if(python_code, IfRewrtiteAction.LOG_IF_BODY)
-        testif_souurce = module_node.body[0].matcher.GetSource()
-        l1_source=module_node.body[0].body[0].matcher.GetSource()
-        l2_source=module_node.body[0].body[1].matcher.GetSource()
-        l3_source=module_node.body[0].body[2].matcher.GetSource()
+        module_node = self.__rewrite_if(python_code, IfRewrtiteConfig('test string'))
         FunWithAst_code = module_node.body[0].matcher.GetSource()
+        lines_original_python = set(python_code.splitlines())
+        lines_fun_with_ast = set(FunWithAst_code.splitlines())
+        diff_lines = lines_fun_with_ast - lines_original_python
+        assert(len(diff_lines)) ==1
+        assert diff_lines.pop() == '    logger.info(\'test string\')'
 
-        #        FunWithAst_code = module_node.matcher.GetSource()
-        assert python_code == FunWithAst_code
-
-    def __rewrite_if(self, python_code, action):
+    def __rewrite_if(self, python_code, config):
         module_node = ast.parse(python_code)
         source_match.GetSource(module_node, python_code)
-        rewrite_if = RewriteIf(action)
+        rewrite_if = RewriteIf(config)
         rewrite_if.visit(module_node)
-        rewrite_if_fixed = ast.fix_missing_locations(module_node)
         return module_node
