75ade49f43aef19d2acc471a4edcfca83608202b
shai <shai_rubin@yahoo.com>
2023-07-01 17:07:18 +0300

    ready for initial release

--- README.md
+++ README.md
@@ -9,9 +9,12 @@ We assume you use python > 3.6
    1. Activate the virtual environment: ```source environment/bin/activate```
 1. Download the test-fun-with-ast project: ```git clone https://github.com/shairubin/test-fun-with-ast```
 1. Install the fun-with-ast library: ```pip install fun-with-ast`
-1.run the example programs:
-    1. ```cd cd test-fun-with-ast/examples/```
-   2. ```python rewrite_if_examples.py``` 
+1. Run the example rewrite programs:
+    1. ```cd test-fun-with-ast/examples/```
+    1. ```python rewrite_if_examples.py``` 
     1. ```python swap_if_else_examples.py``` 
+1. Run the example source match programs 
+   1. ```cd ../preserve_source_examples```
+   1. ```python source_code_preserve_tests.py``` 
 1. Deactivate the virtual environment: `deactivate`
 2. Optional: delete the directory: ```rm -rf ~/example_fun_with_ast```
--- preserve_source_examples/source_code_preserve_tests.py
+++ preserve_source_examples/source_code_preserve_tests.py
@@ -1,6 +1,4 @@
 import ast
-import contextlib
-import os
 import subprocess
 
 from fun_with_ast.manipulate_node.create_node import GetNodeFromInput
@@ -9,56 +7,44 @@ from fun_with_ast.source_matchers.matcher_resolver import GetDynamicMatcher
 from common_utils import CommonUtils, bcolors
 
 
-class TestE2E:
-
-    def test_rewriteIf_no_if1(self, test_program='/home/shai/auto-logging/preserve_source_examples/test_programs/fib.py', output_program='/tmp/test.py'):
-        with contextlib.suppress(FileNotFoundError):
-            os.remove(output_program)
-        out2 = self._perform_sanity(test_program)
-        # read whole file to a string
-        utils = CommonUtils()
-        python_string = utils.read_file_as_string(test_program)
-        print(bcolors.OKCYAN + "\noriginal program:\n" + python_string, bcolors.ENDC)
-        unparsed_program = ast.unparse(ast.parse(python_string))
-        print(bcolors.OKGREEN + "\nunparsed program:\n" + unparsed_program, bcolors.ENDC)
-        fib_node = GetNodeFromInput(python_string)
-        fib_node_matcher = GetDynamicMatcher(fib_node)
-        fib_node_matcher.do_match(python_string)
-        fun_with_ast_source = fib_node_matcher.GetSource()
 
 
-    #        python_result = self._rewrite_tree(python_string)
-#        rewrite_out = self._run_modified_program(python_result, output_program)
-#        self._comparte_asts(test_program, output_program)
-#        assert out2.stdout == rewrite_out.stdout
+def match_original_program(test_program='./test_programs/fib.py'):
+    _perform_sanity(test_program)
+    # read whole file to a string
+    utils = CommonUtils()
+    python_program_as_string = utils.read_file_as_string(test_program)
+    print(bcolors.OKCYAN + "\nORIGINAL PROGRAM:\n" + python_program_as_string, bcolors.ENDC)
+    unparsed_program = ast.unparse(ast.parse(python_program_as_string))
+    print(bcolors.OKGREEN + "\nAST UNPARSED PROGRAM:\n" + unparsed_program, bcolors.ENDC)
+    fib_node = GetNodeFromInput(python_program_as_string, 0, get_module=True)
+    fib_node_matcher = GetDynamicMatcher(fib_node)
+    fib_node_matcher.do_match(python_program_as_string)
+    fun_with_ast_source = fib_node_matcher.GetSource()
+    print(bcolors.OKBLUE + "\nFUN WITH AST PROGRAM:\n" + fun_with_ast_source, bcolors.ENDC)
+    assert fun_with_ast_source == python_program_as_string
 
-    def _run_modified_program(self, python_result, output_program):
-        with open(output_program, 'w') as modified_program:
-            modified_program.write(python_result)
-        rewrite_out = subprocess.run(["python", output_program], stdout=subprocess.PIPE)
-        return rewrite_out
 
-    def _rewrite_tree(self, python_string):
-        parsed_tree = simple_parse_example(python_string)
-        simple_rewrite_import_example(parsed_tree)
-        rewrite_if_tree = simple_rewrtie_if_example(parsed_tree)
-        python_result = simple_unparse_example(rewrite_if_tree)
-        logging.info(python_result)
-        return python_result
 
-    def _perform_sanity(self, test_program):
-        out1 = subprocess.run(["python", test_program],
-                              stdout=subprocess.PIPE)
-        out2 = subprocess.run(["python", test_program],
-                              stdout=subprocess.PIPE)
-        assert out2.stdout == out1.stdout
-        return out2
+def _perform_sanity(test_program):
+    out1 = subprocess.run(["python", test_program],
+                          stdout=subprocess.PIPE)
+    out2 = subprocess.run(["python", test_program],
+                          stdout=subprocess.PIPE)
+    assert out2.stdout == out1.stdout
+    return out2
 
-    def _comparte_asts(self, test_program, output_program):
-        test_utils = TestUtils()
-        rewrite_string = subprocess.run(["grep", "-v", "info", output_program], stdout=subprocess.PIPE)
-        original_string = test_utils.read_file_as_string(test_program)
-        original_ast = simple_parse_example(original_string)
-        rewrite_ast = simple_parse_example(rewrite_string.stdout)
-        assert test_utils.compare_ast(original_ast, rewrite_ast)
+def _comparte_asts(test_program, output_program):
+    utils = CommonUtils()
+    rewrite_string = subprocess.run(["grep", "-v", "info", output_program], stdout=subprocess.PIPE)
+    original_string = utils.read_file_as_string(test_program)
+    original_ast = 'TBD'
+    rewrite_ast = 'TBD'
+    assert utils.compare_ast(original_ast, rewrite_ast)
 
+
+
+if __name__ == "__main__":
+    test_programs = ['./test_programs/fib.py']
+    for p in test_programs:
+        match_original_program(test_program=p)
