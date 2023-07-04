adf1818c0dcc6d629d05e8336cf28775cc4d98cf
nandofromthebando <87321214+nandofromthebando@users.noreply.github.com>
2023-07-04 13:54:49 -0700

    Update prime.py

--- preserve_source_examples/test_programs/prime.py
+++ preserve_source_examples/test_programs/prime.py
@@ -2,24 +2,19 @@
 
 
 def isPrime(x):
-    check = False
-    if (x ==1):
-        check = True
-        return check
-    elif x > 1:
+    if (x < 2):
+        return True
+    else:
         for i in range( 2, x):
             if (x % i == 0):
-                check = True
-                break
-            else:
-                check = False
-    return check
+                return False
+    return True
 
 
 # checker that was used in fib.py
+print('fun with ast')
 for i in range(1, 15):
-    if isPrime(i) == True:
-        print('fun with ast')
+    if isPrime(i):
         print(i, 'is a Prime Number')
     else:
         print(i, 'is a not Prime Number')
