3a37e360bb340da347339b597a1e428e114eafff
shai <shai_rubin@yahoo.com>
2023-07-21 11:13:37 +0300

    hello world

---
+++ preserve_source_examples/hello_wolrd.py
@@ -0,0 +1,15 @@
+import ast
+import subprocess
+
+from fun_with_ast.manipulate_node.get_node_from_input import GetNodeFromInput
+from fun_with_ast.source_matchers.matcher_resolver import GetDynamicMatcher
+
+from common_utils import CommonUtils, bcolors
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("hello world")
