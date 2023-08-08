e5ead3b67ce2974aca6cf6036fd689c598210e6e
shai <shai_rubin@yahoo.com>
2023-08-08 18:06:02 +0300

    the smallest program that still fails

---
+++ work_in_progress/vqgan_arch_modified.py
@@ -0,0 +1,7 @@
+
+def tmp(self, img_size):
+        logger.info(f"vqgan is loaded from: {model_path} [params_ema]")
+        #elif "params" in chkpt:
+        #    self.load_state_dict(torch.load(model_path, map_location="cpu")["params"])
+        #    #logger.info(f"vqgan is loaded from: {model_path} [params]")
+
