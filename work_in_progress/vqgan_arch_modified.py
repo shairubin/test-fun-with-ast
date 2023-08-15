c62cda1791f51205a440fa883fd4a589bb4c474a
shai <shai_rubin@yahoo.com>
2023-08-15 10:35:06 +0300

    file modelling.py failed to pass

--- work_in_progress/vqgan_arch_modified.py
+++
@@ -1,7 +0,0 @@
-
-def tmp(self, img_size):
-        logger.info(f"vqgan is loaded from: {model_path} [params_ema]")
-        #elif "params" in chkpt:
-        #    self.load_state_dict(torch.load(model_path, map_location="cpu")["params"])
-        #    #logger.info(f"vqgan is loaded from: {model_path} [params]")
-
