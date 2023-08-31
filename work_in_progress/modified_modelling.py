61338eda4cafbf0203fb940dda485548ca8f6961
shai <shai_rubin@yahoo.com>
2023-08-31 20:39:52 +0300

    modified_modelling.py reproduced error

--- work_in_progress/modified_modelling.py
+++ work_in_progress/modified_modelling.py
@@ -1,34 +1,12 @@
 
 
 def norm(type, *args, **kwargs):
-    if type == "rmsnorm":
-        return RMSNorm(*args, **kwargs)
-    elif type == "layernorm":
-        return nn.LayerNorm(*args, **kwargs)
-    else:
+    if True:
         raise ValueError(f"Unknown norm type {type}")
 
-
 def dot_product_attention_weights(
-    query: Any,
-    key: Any,
-    bias: Optional[Any] = None,
-    mask: Optional[Any] = None,
-    embed_pos: Optional[Any] = None,
-    broadcast_dropout: bool = True,
-    dropout_rng: Optional[PRNGKey] = None,
-    dropout_rate: float = 0.0,
-    deterministic: bool = False,
-    dtype: Any = jnp.float32,
-    precision: PrecisionLike = None,
-    sinkhorn_iters: int = 1,
-    is_encoder: bool = False,
-    tau=None,
 ):
     """
-    Computes dot-product attention weights given query and key.
-    mask is included into the bias.
-
     Adapted from flax.linen.attention.dot_product_attention_weights"
     """
 
