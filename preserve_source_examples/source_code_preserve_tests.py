bb953edb43e24e9e1fac1fd4b98fcffb554c98e1
shai <shai_rubin@yahoo.com>
2024-02-16 16:13:27 +0200

    issue reproduced in source 1

--- preserve_source_examples/source_code_preserve_tests.py
+++ preserve_source_examples/source_code_preserve_tests.py
@@ -159,7 +159,7 @@ if __name__ == "__main__":
 
     ]
     wip_programs = [
-                    ('/home/shai/test_fun_with_ast/work_in_progress/source.py', False),
+                    ('/home/shai/test_fun_with_ast/work_in_progress/source1.py', False),
                     ]
     if RUN_TEST_PROGRAMS:
         _run_on_example_programs(test_programs)
