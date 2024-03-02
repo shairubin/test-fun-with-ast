32f4e5f098f44c031b83a679de993b7a0a60ff6a
shai <shai_rubin@yahoo.com>
2024-03-02 22:26:00 +0200

    issue reproduced seems like issue is white spaces at the end of a line

--- preserve_source_examples/source_code_preserve_tests.py
+++ preserve_source_examples/source_code_preserve_tests.py
@@ -173,7 +173,8 @@ if __name__ == "__main__":
 
     ]
     wip_programs = [
-                    ('/home/shai/test_fun_with_ast/work_in_progress/the_game.py', False),
+                    ('/home/shai/test_fun_with_ast/work_in_progress/the_game1.py', False),
+                    #('/home/shai/make-developers-brighter/e2e_tests/test_files/TheGame/the_game.py', False),
                     ]
     if RUN_TEST_PROGRAMS:
         _run_on_example_programs(test_programs)
---
+++ work_in_progress/the_game1.py
@@ -0,0 +1,5 @@
+def log_one_game(game_number: int, cost_function, seed : int = None) -> None:
+
+    table = Table(show_header=True, header_style="bold magenta")
+        
+    pass
