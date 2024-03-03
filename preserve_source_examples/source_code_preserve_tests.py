cd2d1ad1727739a7b885c114315b068e1b4b2e95
shai <shai_rubin@yahoo.com>
2024-03-03 11:21:38 +0200

    v 0.2.78 58 tests pass

--- preserve_source_examples/source_code_preserve_tests.py
+++ preserve_source_examples/source_code_preserve_tests.py
@@ -107,8 +107,8 @@ def _run_on_example_programs(test_programs):
         print('TEST NUMBER: ' + str(index + 1) + ' END FOR: ' + p[0])
 
 
-RUN_TEST_PROGRAMS = False
-RUN_WIP_PROGRAMS = True
+RUN_TEST_PROGRAMS = True
+RUN_WIP_PROGRAMS = False
 
 
 if __name__ == "__main__":
@@ -170,10 +170,11 @@ if __name__ == "__main__":
                     ('./test_programs/test_dlpack.py', False),
                     ('./test_programs/annotations.py', False),
                     ('./test_programs/heap.py', False),
+                    ('./test_programs/the_game.py', False),
 
     ]
     wip_programs = [
-                    ('/home/shai/test_fun_with_ast/work_in_progress/the_game1.py', False),
+                    ('/home/shai/test_fun_with_ast/work_in_progress/the_game.py', False),
                     #('/home/shai/make-developers-brighter/e2e_tests/test_files/TheGame/the_game.py', False),
                     ]
     if RUN_TEST_PROGRAMS:
--- work_in_progress/the_game1.py
+++
@@ -1,4 +0,0 @@
-def log_one_game(game_number) -> None:
-    table = Table(show_header=True, header_style="bold magenta")
-        
-    pass
