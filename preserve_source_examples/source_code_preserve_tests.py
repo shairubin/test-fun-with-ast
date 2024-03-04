54305b3ed9c4eba726a21b4712c8cf23a5fca120
shai <shai_rubin@yahoo.com>
2024-03-04 13:40:49 +0200

    60 tests pass v 0.2.78

--- preserve_source_examples/source_code_preserve_tests.py
+++ preserve_source_examples/source_code_preserve_tests.py
@@ -107,8 +107,8 @@ def _run_on_example_programs(test_programs):
         print('TEST NUMBER: ' + str(index + 1) + ' END FOR: ' + p[0])
 
 
-RUN_TEST_PROGRAMS = False
-RUN_WIP_PROGRAMS = True
+RUN_TEST_PROGRAMS = True
+RUN_WIP_PROGRAMS = False
 
 
 if __name__ == "__main__":
@@ -172,6 +172,7 @@ if __name__ == "__main__":
                     ('./test_programs/heap.py', False),
                     ('./test_programs/the_game.py', False),
                     ('./test_programs/decomposition.py', False),
+                    ('./test_programs/ensure_clipped_test.py', False),
 
     ]
     wip_programs = [
