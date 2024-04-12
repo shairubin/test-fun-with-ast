60390dcd1d8de06fca020e6e9035f9bfc71e7441
shai <shai_rubin@yahoo.com>
2024-04-12 15:05:55 +0300

    issue reproduced

--- preserve_source_examples/source_code_preserve_tests.py
+++ preserve_source_examples/source_code_preserve_tests.py
@@ -107,8 +107,8 @@ def _run_on_example_programs(test_programs):
         print('TEST NUMBER: ' + str(index + 1) + ' END FOR: ' + p[0])
 
 
-RUN_TEST_PROGRAMS = True
-RUN_WIP_PROGRAMS = False
+RUN_TEST_PROGRAMS = False
+RUN_WIP_PROGRAMS = True
 
 
 if __name__ == "__main__":
@@ -177,7 +177,7 @@ if __name__ == "__main__":
 
     ]
     wip_programs = [
-                    ('/home/shai/test_fun_with_ast/work_in_progress/Journal1.py', False),
+                    ('/home/shai/test_fun_with_ast/work_in_progress/autotune_process.py', False),
                     #('/home/shai/jrnl/jrnl/journals/Journal.py', False),
                     ]
     if RUN_TEST_PROGRAMS:
