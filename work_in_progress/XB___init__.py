f9e654d2463e72b919a3452448097bcf8fdeb8f2
shai <shai_rubin@yahoo.com>
2024-05-10 11:21:09 +0300

    issue reproduced

--- work_in_progress/XB___init__.py
+++ work_in_progress/XB___init__.py
@@ -1,4 +1,4 @@
 def get_sharding_prop_cache_info():
     return (
-        DTensor._propagator.propagate_op_sharding.cache_info()  # type:ignore[attr-defined]
+        DTensor._propagator.propagate_op_sharding.cache_info()  # type:ignore[attr-defined
     )
